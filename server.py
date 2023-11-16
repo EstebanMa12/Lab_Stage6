259cf021301906ed8cf90279685b88c14684d183
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-16 01:31:35 -0500

    Update client and server code to use msg_length and DISCONNECT_MESSAGE

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 import socket
 import threading
-import time
 
+
+HEADER = 64
 PORT = 5050
 # SERVER = "192.168.1.20"
 SERVER = socket.gethostbyname(socket.gethostname())
 # print(socket.gethostname())
 ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -15,12 +18,17 @@ def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
     while connected:
-        msg = conn.recv(1024).decode()
-        if msg == "quit":
-            connected = False
-        print(f"[{addr}] {msg}")
-        conn.send("Message received".encode())
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+          msg_length = int(msg_length)
+          msg = conn.recv(msg_length).decode(FORMAT)
+          if msg == DISCONNECT_MESSAGE:
+              connected = False
+          print(f"[{addr}] {msg}")
+
     conn.close()
+
+
 
 def start():
     server.listen()
