1819cba3f99d8cce1521ae7cbf121d0144dddb04
EstebanMa12 <desteban.maya@udea.edu.co>
2023-11-17 10:09:49 -0500

    This commit removes all code from both the client and server python scripts.

---
+++ C/lab.h
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+#define ISVALIDSOCKET(s) ((s)>=0)
+#define SOCKET int
+#define CLOSESOCKET(s) close(s)
+#define GETSOCKETERRNO() (errno)
+
+#include <stdio.h>
+#include <string.h>
